af07239f0a6ad6a8e1225029b33b50a3f5a037f8
Simon Gribert <simon.gribert@live.se>
2022-10-30 15:13:56 +0100

    Initial Commit

---
+++ todoswift/todoswift/todoswiftApp.swift
@@ -0,0 +1,18 @@
+//
+//  todoswiftApp.swift
+//  todoswift
+//
+//  Created by Simon Gribert on 2022-10-22.
+//
+
+import SwiftUI
+
+@main
+struct todoswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            TaskView()
+        }
+    }
+}
